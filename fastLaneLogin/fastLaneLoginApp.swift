10165b73ab8fd7e8a4115b27da295f85e82ac7bf
Umar Farooq Nadeem <dev.omar561@gmail.com>
2024-09-12 17:46:13 +0500

    CircleCI sample
    Sample project created

---
+++ fastLaneLogin/fastLaneLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastLaneLoginApp.swift
+//  fastLaneLogin
+//
+//  Created by iCode on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct fastLaneLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
